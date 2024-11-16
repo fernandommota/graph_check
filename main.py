e354b23cd408c58f37419402a29d086efa52afe8
fernandommota <mota.fernandomaia@gmail.com>
2024-11-16 07:40:05 -0400

    updates

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 dataset = load_dataset("lytang/LLM-AggreFact",token=config["HUGGINGFACE_ACCESS_TOKEN"])
+models = []
+for item in dataset["test"]:
+    models.append(item["dataset"])
+#print('models',list(dict.fromkeys(models)))
 
-def query_llm(document, claim):
+def naive_query_llm(document, claim):
     #llama3.2, mixtral:8x7b
     response = ollama.chat(model='mixtral:8x7b', messages=[
     {
@@ -27,35 +31,111 @@ def query_llm(document, claim):
     },
     ])
     return response['message']['content']
-#query_llm()
 
-#print(dataset)
-#print(dir(dataset["test"]))
-#print(len(dataset["test"]))
-
-def rag_item(item):
+def naive_approach_prompt(item):
         
-    result = query_llm(item["doc"], item["claim"])
-    print(f'Pid: {os.getpid()} - result for {item["contamination_identifier"]}', result[0:15])
+    response = naive_query_llm(item["doc"], item["claim"])
+    #print(f'Pid: {os.getpid()} - result for {item["contamination_identifier"]}', result[0:15])
 
     return [
         item["dataset"], #  dataset
         item["contamination_identifier"], # dataset:id
         item["label"], # class
-        result # predicted # 1 if result[0:5].strip() == 'True' else 0
+        response.strip()[0:4] # predicted # 1 if result[0:5].strip() == 'True' else 0
     ]
 
-items = list(filter(lambda item: item["dataset"] == "ClaimVerify", dataset["test"]))
-pool_obj = multiprocessing.Pool(12)
-results = []
-results.append(pool_obj.map(rag_item,items[0:3]))#items[0:3]
-pool_obj.close()
+def cot_query_llm(document, claim):
+    #llama3.2, mixtral:8x7b
+    response = ollama.chat(model='mixtral:8x7b', messages=[
+    {
+        'role': 'user',
+        'content': f'''
+            Convert the given the context to a graph. Given the context as a graph and not prior knowledge, check if the follow claim is true or false. Response only with true or false.
+            Example:
+                Context: John is male and has 32 years, he is marriage with Mary, female and has 28 years.
+                Graph Data:
+                    nodes: John, male, 32 years, Mary
+                    relationships: 
+                        - John -> marriage -> Mary
+                        - John -> sex -> Male
+                        - John -> age -> 32 years
+                        - Mary -> marriage -> John
+                        - Mary -> sex -> Female
+                        - Mary -> age -> 28 years
+                Claim: Mary has 30 years and is John's sister.
+                Correct answer: False
+            User prompt:
+                Context information is below.
+                ---------------------
+                {document}
+                ---------------------
+                Claim: {claim}
+        ''',
+    },
+    ])
+    return response['message']['content']
 
-print(f'results len: {len(results[0])}')
-print(f'results', results[0])
-df = DataFrame(data=np.array(results[0]), 
-               index=np.arange(len(results[0])), 
-               columns=['dataset','id','label','predicted'])
+def cot_approach_prompt(item):
+        
+    response = cot_query_llm(item["doc"], item["claim"])
+    print(f'Pid: {os.getpid()} - result for {item["contamination_identifier"]}', response[0:150])
 
-df.to_csv('output/output.csv', index=False)  
-print(df)
+    return [
+        item["dataset"], #  dataset
+        item["contamination_identifier"], # dataset:id
+        item["label"], # class
+        response.strip()[0:4] # predicted # 1 if result[0:5].strip() == 'True' else 0
+    ]
+
+# setup experiment
+experiments = [
+    {
+        "model": "mixtral:8x7b",
+        "model_alias": "mixtral_8x7b",
+        "approachs": [
+            #"Naive",
+            "CoT"
+        ],
+        "datasets":[
+            "AggreFact-CNN",
+            #"AggreFact-XSum",
+            #"TofuEval-MediaS",
+            #"TofuEval-MeetB",
+            #"Wice",
+            #"Reveal",
+            #"ClaimVerify",
+            #"FactCheck-GPT",
+            #"ExpertQA",
+            #"Lfqa",
+            #"RAGTruth"
+        ]
+    }
+]
+
+for experiment in experiments:
+    model = experiment["model"]
+    model_alias = experiment["model_alias"]
+    for approach in experiment["approachs"]:
+        for dataset_name in experiment["datasets"]:
+            items = list(filter(lambda item: item["dataset"] == dataset_name, dataset["test"]))
+            pool_obj = multiprocessing.Pool(12)
+            results = []
+            if approach == "Naive":
+                results.append(pool_obj.map(naive_approach_prompt,items))#items[0:3]
+            elif approach == "CoT":
+                results.append(pool_obj.map(cot_approach_prompt,items))#items[0:3]
+            pool_obj.close()
+            
+            for result in results[0]:
+                result.insert(0, approach)
+                result.insert(0, model)
+                
+            df = DataFrame(data=np.array(results[0]), 
+                        index=np.arange(len(results[0])), 
+                        columns=['model','approach','dataset','id','label','response'])
+
+            output_folder = f'output/{model_alias}/{approach}'
+            if not os.path.exists(output_folder):
+                os.mkdir(output_folder)
+            df.to_csv(f'{output_folder}/{dataset_name}.csv', index=False)  
+            print(df)
--- results.py
+++ results.py
@@ -1,23 +1,107 @@
 import pandas as pd
+import glob
+import os
 
 def get_result(row):
     y = row['label']
-    predicted = '1' if row['predicted'].strip()[0:4] == 'True' else '0'
-    print (f'label: {y} - predicted: {predicted}')
+    predicted = '1' if row['response'] == 'True' else '0'
+    #print (f'label: {y} - predicted: {predicted}')
     # 1 if result[0:5].strip() == 'True' else 0
     if y == predicted:
         return 1
     else:
         return 0
     
-df = pd.read_csv('output.csv', names=['dataset','id','label','predicted'], header=None)
+all_files = glob.glob("output/*/*/*.csv")
+dfs = []
+for filename in all_files:
+    df = pd.read_csv(filename, names=['model','approach','dataset','id','label','response'], header=None)
+    dfs.append(df)
+
+df = pd.concat(dfs, axis=0, ignore_index=True)
 
 df['result'] = df.apply(get_result, axis=1)
 
-print(df)  
+df = pd.pivot_table(df, values='result', index=['model','approach'],
+                       columns=['dataset'], aggfunc={'result': ["sum","count"]}, fill_value=0).reset_index()
+print(df.info()) 
+print(df.columns) 
+df.columns = [
+    "Model"
+    ,"Approach"
+    #count
+    ,"AggreFact-CNN_count"
+    ,"AggreFact-XSum_count"
+    ,"ClaimVerify_count"
+    ,"ExpertQA_count"
+    ,"FactCheck-GPT_count"
+    ,"Lfqa_count"
+    ,"Reveal_count"
+    ,"TofuEval-MediaS_count"
+    ,"TofuEval-MeetB_count"
+    ,"Wice_count"
+    ,"dataset_count"
+    #sum
+    ,"AggreFact-CNN_sum"
+    ,"AggreFact-XSum_sum"
+    ,"ClaimVerify_sum"
+    ,"ExpertQA_sum"
+    ,"FactCheck-GPT_sum"
+    ,"Reveal_sum"
+    ,"Lfqa_sum"
+    ,"TofuEval-MediaS_sum"
+    ,"TofuEval-MeetB_sum"
+    ,"Wice_sum"
+    ,"dataset_sum"
+]
+df['AggreFact-CNN_acc'] = df['AggreFact-CNN_sum'] / df['AggreFact-CNN_count']
+df['AggreFact-XSum_acc'] = df['AggreFact-XSum_sum'] / df['AggreFact-XSum_count']
+df['TofuEval-MediaS_acc'] = df['TofuEval-MediaS_sum'] / df['TofuEval-MediaS_count']
+df['TofuEval-MeetB_acc'] = df['TofuEval-MeetB_sum'] / df['TofuEval-MeetB_count']
+df['Wice_acc'] = df['Wice_sum'] / df['Wice_count']
+df['Reveal_acc'] = df['Reveal_sum'] / df['Reveal_count']
+df['ClaimVerify_acc'] = df['ClaimVerify_sum'] / df['ClaimVerify_count']
+df['FactCheck-GPT_acc'] = df['FactCheck-GPT_sum'] / df['FactCheck-GPT_count']
+df['ExpertQA_acc'] = df['ExpertQA_sum'] / df['ExpertQA_count']
+df['Lfqa_acc'] = df['Lfqa_sum'] / df['Lfqa_count']
 
+df = df[[
+    "Model"
+    ,"Approach"
+    ,"AggreFact-CNN_count"
+    ,"AggreFact-CNN_sum"
+    ,"AggreFact-CNN_acc"
+    ,"AggreFact-XSum_count"
+    ,"AggreFact-XSum_sum"
+    ,"AggreFact-XSum_acc"
+    ,"TofuEval-MediaS_count"
+    ,"TofuEval-MediaS_sum"
+    ,"TofuEval-MediaS_acc"
+    ,"TofuEval-MeetB_count"
+    ,"TofuEval-MeetB_sum"
+    ,"TofuEval-MeetB_acc"
+    ,"Wice_count"
+    ,"Wice_sum"
+    ,"Wice_acc"
+    ,"Reveal_count"
+    ,"Reveal_sum"
+    ,"Reveal_acc"
+    ,"ClaimVerify_count"
+    ,"ClaimVerify_sum"
+    ,"ClaimVerify_acc"
+    ,"FactCheck-GPT_count"
+    ,"FactCheck-GPT_sum"
+    ,"FactCheck-GPT_acc"
+    ,"ExpertQA_count"
+    ,"ExpertQA_sum"
+    ,"ExpertQA_acc"
+    ,"Lfqa_count"
+    ,"Lfqa_sum"
+    ,"Lfqa_acc"
+]]
 
-print(df.groupby(['result']).count()) 
+print(df)
+df.to_csv(f'reports/mixtral_8x7b/result.csv')
 
 ###
 #  llama3.2 - AggreFact-CNN (1.5% da amostra) - 559 rows
